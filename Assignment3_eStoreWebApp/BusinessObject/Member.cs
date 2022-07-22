ef5cc17507a3754fa3b60a2fb5f41fdb8c7c085e
binhlqse150199@fpt.edu.vn <binhlqse150199@fpt.edu.vn>
2022-07-22 22:57:41 +0700

    Display

--- Assignment3_eStoreWebApp/BusinessObject/Member.cs
+++ Assignment3_eStoreWebApp/BusinessObject/Member.cs
@@ -15,11 +15,17 @@ namespace BusinessObject
         }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Member ID")]
         public int MemberId { get; set; }
+        [Display(Name = "Email")]
         public string Email { get; set; }
+        [Display(Name = "Company Name")]
         public string CompanyName { get; set; }
+        [Display(Name = "City")]
         public string City { get; set; }
+        [Display(Name = "Country")]
         public string Country { get; set; }
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         public virtual ICollection<Order> Orders { get; set; }
--- Assignment3_eStoreWebApp/BusinessObject/OrderDetail.cs
+++ Assignment3_eStoreWebApp/BusinessObject/OrderDetail.cs
@@ -7,12 +7,19 @@ namespace BusinessObject
 {
     public partial class OrderDetail
     {
+        [Display(Name = "Order ID")]
         public int OrderId { get; set; }
+        [Display(Name = "Product ID")]
         public int ProductId { get; set; }
+        [Display(Name = "Unit Price")]
         public decimal UnitPrice { get; set; }
+        [Display(Name = "Quantity")]
         public int Quantity { get; set; }
+        [Display(Name = "Discount")]
         public double Discount { get; set; }
+        [Display(Name = "Order")]
         public virtual Order Order { get; set; }
+        [Display(Name = "Product")]
         public virtual Product Product { get; set; }
     }
 }
